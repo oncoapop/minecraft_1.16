6f0e37dfa2b9dfe21f0e994b4bf4aa772a705a8c
oncoapop <oncoapop@sdf.org>
2020-07-22 01:52:13 -0700

    Add mod start and setup

---
+++ mod_start.sh
@@ -0,0 +1,3 @@
+java -server -XX:+UseConcMarkSweepGC -XX:ParallelGCThreads=2 -XX:+AggressiveOpts -Xmx4G -jar spigot-1.16.1.jar nogui 
+
+
---
+++ setup_minecraft.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Reference https://minecraft.gamepedia.com/Tutorials/Setting_up_a_server
+
+
+# Copy link from https://www.minecraft.net/en-us/download/server/
+wget  https://launcher.mojang.com/v1/objects/a412fd69db1f81db3f511c1463fd304675244077/server.jar
+
+# Install Java
+sudo apt install openjdk-8-jre-headless
